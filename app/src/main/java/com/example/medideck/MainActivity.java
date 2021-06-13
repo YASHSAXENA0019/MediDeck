d0b7da800b2d48b156d5cd6958749eaa0069a98f
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-06-13 18:00:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/medideck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.medideck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
