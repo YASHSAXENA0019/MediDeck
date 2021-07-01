af695c84429012d8ae4c2a107322f62136bc65d6
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-07-01 09:58:25 +0530

    222

---
+++ app/src/main/java/com/example/medideck/doc_or_user.java
@@ -0,0 +1,14 @@
+package com.example.medideck;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class doc_or_user extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doc_or_user);
+    }
+
+}
