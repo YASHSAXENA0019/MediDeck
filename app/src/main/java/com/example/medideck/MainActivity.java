c61fd076f1b08f5543ad4427fe9a4433bbe1df29
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-07-01 09:45:07 +0530

    gg

--- app/src/main/java/com/example/medideck/MainActivity.java
+++ app/src/main/java/com/example/medideck/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.medideck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-//sahil
-    @Override
-    //yash
-    // hemlo
+    private ImageButton profile;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_home_page);
+        profile = (ImageButton) findViewById(R.id.imageButton1);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUser();
+            }
+        });
+
     }
+
+    private void openUser() {
+        Intent intent=new Intent(this, user_profile.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/medideck/user_profile.java
@@ -0,0 +1,14 @@
+package com.example.medideck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
