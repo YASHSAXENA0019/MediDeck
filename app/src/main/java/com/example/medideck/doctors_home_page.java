d2ebe86d826d94b66758ee863e53639548dd3231
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-07-01 16:17:30 +0530

    some more connections

---
+++ app/src/main/java/com/example/medideck/doctors_home_page.java
@@ -0,0 +1,13 @@
+package com.example.medideck;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class doctors_home_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctors_home_page);
+    }
+}
